a391935addacdccdb34fef145b379af8b9889a33
Dexter <dexterong94@gmail.com>
2017-05-20 11:43:32 +0800

    syntax

--- camera.py
+++ camera.py
@@ -1,17 +1,15 @@
-# Capture image
+from google.cloud import storage
 from picamera import PiCamera
 
+# Capture image
 camera = PiCamera()
 camera.capture('/home/pi/Desktop/window.jpg')
 
 
 # Upload image
-from google.cloud import storage
-
 client = storage.Client()
 
 bucket = client.get_bucket('smartwindow-91b1c.appspot.com')
 
 imageBlob = bucket.get_blob('window.jpg')
 imageBlob.upload_from_filename(filename='/home/pi/Desktop/window.jpg')
-//
